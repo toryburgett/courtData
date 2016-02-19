a184b78caea1a53ad47124af38a8ef1fca308ec1
Tory Burgett <vdburgett@gmail.com>
2016-02-19 14:04:16 -0500

    matched author of joined opinion to joined justice

--- wiki/script.js
+++ wiki/script.js
@@ -29,11 +29,6 @@ $(document).ready(function(){
     return saveVar;
   }
 
-  function mapWordToLetterArray(){
-
-  }
-
-
   function wikiAjax(){
     var url = wikiJson;
     $.ajax({
@@ -57,16 +52,139 @@ $(document).ready(function(){
         }else if(response[a].includes("}}")){
           rowEndIndex = a;
           var caseLength = rowEndIndex - rowStartIndex;
-          var newCase = {case: "", tempArray: []};
+          var newCase = {case: "", volume: "", argue_date: "", decision_date: "", justices: [], opinions: [], };
 
           for(var b=0; b<caseLength; b++){
             // var newCaseEntry = "";
+            var caseNum = "|#=";
+            var caseInfoKeys = ["case", "volume", "argue-date", "decision-date", "case-display"];
+            var justiceRow = "|justice";
+            var justiceKeys = ["roberts", "scalia", "kennedy", "thomas", "ginsburg", "breyer", "alito", "sotomayor", "kagan"];
+            var opinionTypes = ["majority", "dissent", "concurrence", "concurrencedissent", "plurality", "didnotparticipate"];
 
-            if(response[rowStartIndex+b].includes("|case=")){
-              newCase.case = deleteText(response[rowStartIndex+b], ["|case="]);
-            }else{
-              var newCaseEntry = response[rowStartIndex+b];
-              newCase.tempArray.push(newCaseEntry);
+
+
+            for(var c=0; c<caseInfoKeys.length; c++){
+              if(response[rowStartIndex+b].includes("|"+caseInfoKeys[c]+"=")){
+                var newCaseKey = caseInfoKeys[c];
+                if(caseInfoKeys[c].includes("-")){
+                  newCaseKey = caseInfoKeys[c].replace("-", "_");
+                }
+                if(newCaseKey === "case_display"){
+                  newCaseKey = "case";
+                }
+                newCase[newCaseKey] = deleteText(response[rowStartIndex+b], [("|"+caseInfoKeys[c]+"=")]);
+              }
+            }
+            for(var d=0; d<justiceKeys.length; d++){
+              if(response[rowStartIndex+b].includes("<!--"+justiceKeys[d]+"-->")){
+                var rawCurrentJustice = response[rowStartIndex+b];
+                var editCurrentJustice = deleteText(rawCurrentJustice, ["<!--"+justiceKeys[d]+"-->"]);
+                var currentJusticeArray = editCurrentJustice.split("|");
+
+
+                var justiceName = justiceKeys[d];
+                var justiceVote = "";
+                var justiceAttendance = "";
+                var justiceAuthor = "";
+                var justiceInfo = {name: justiceName, vote: justiceVote, author: justiceAuthor, joined: {total: [], inFull: [], part: []}, attendance: justiceAttendance, data: editCurrentJustice};
+
+                var numOpinionsJoin = editCurrentJustice.match(/opinion/g).length;
+
+                for(var e=0; e<currentJusticeArray.length; e++){
+                  //if the justiceArray entry pertains to an opinion
+                  if(currentJusticeArray[e].includes("opinion")){
+
+                    if(currentJusticeArray[e].includes("join")){
+                      // if joined opinion
+                      var justiceJoined = {opinion: "", author: "", partJoin: 0, fullJoin: 0};
+                      var justiceJoinedAuthor = "";
+                      var rawJusticeOpinion = currentJusticeArray[e];
+                      var opinion = "";
+
+                      // what is the opinionType of this?
+                      for(var f = 0; f<opinionTypes.length; f++){
+                        if(rawJusticeOpinion.includes(opinionTypes[f])){
+                          opinion = opinionTypes[f];
+                          justiceJoined.opinion = opinion;
+                        }
+                      }
+
+
+
+
+
+
+
+
+
+                      var authorIdArray = currentJusticeArray[e].split(opinion);
+                      var authorJoined = "";
+                      // console.log("="+opinion+authorIdArray[1]);
+
+                      // who is the author of joined opinion?
+                      for(var g = 0; g<caseLength; g++){
+                        if(response[rowStartIndex+g].includes("="+opinion+authorIdArray[1])){
+                          console.log("WE DID IT");
+                          for(var h=0; h<justiceKeys.length; h++){
+                            if(response[rowStartIndex+g].includes(justiceKeys[h])){
+                              authorJoined = justiceKeys[h];
+                              console.log(authorJoined);
+                              justiceJoined.author = justiceKeys[h];
+                              console.log("DID IT");
+                            }
+                          }
+                        }
+                      }
+
+                      console.log(authorJoined);
+
+
+
+
+
+
+
+                      //Did they join the whole thing, or just partially?
+                      if(rawJusticeOpinion.includes("partjoin")){
+                        // if joind in part
+                        justiceJoined.partJoin = 1;
+                        justiceInfo.joined.part.push(justiceJoined);
+                      }else{
+                        // if they joined the entire thing
+                        justiceJoined.fullJoin = 1;
+                        justiceInfo.joined.inFull.push(justiceJoined);
+                      }
+
+
+                      justiceInfo.joined.total.push(justiceJoined);
+
+                    }else{
+                      // if authored opinion
+                      justiceInfo.author = currentJusticeArray[e];
+
+
+                    }
+
+                  }
+
+
+
+                }
+
+
+
+
+
+
+
+
+                newCase.justices.push(justiceInfo);
+
+// [newCaseKey] = deleteText(response[rowStartIndex+b], [("|"+[d]+"=")]);
+
+              }
+
             }
 
           }
